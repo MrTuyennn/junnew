aa81ff3e5b71e916f59581e3d6e8a7be3411eb8c
juncook <nguyenngoctuyen188@gmail.com>
2025-09-30 13:13:16 +0700

    feat: add font app && implement theme app

---
+++ app/src/main/java/com/junnew/design_system/theme/AppFonts.kt
@@ -0,0 +1,18 @@
+package com.junnew.design_system.theme
+
+import com.junnew.R
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+
+
+val fireFlyFonts = FontFamily(
+    Font(R.font.light, FontWeight.Light),
+    Font(R.font.thin, FontWeight.Thin),
+    Font(R.font.regular, FontWeight.Normal),
+    Font(R.font.medium, FontWeight.Medium),
+    Font(R.font.bold, FontWeight.SemiBold),
+    Font(R.font.extra_bold, FontWeight.Black),
+    Font(R.font.itallic, FontWeight.Normal, FontStyle.Italic),
+)
